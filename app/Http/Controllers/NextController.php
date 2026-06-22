6d8d0be4e29eacfafc9360e4eecd344a143a0a8e
hkiira <achkar.abder@gmail.com>
2026-06-22 20:15:52 +0100

    Set image to null for colors if product variation image is missing

--- app/Http/Controllers/NextController.php
+++ app/Http/Controllers/NextController.php
@@ -886,9 +886,6 @@ class NextController extends Controller
     {
         $attributesGrouped = [];
 
-        $principalImg = $product->principalImage->first() ?? $product->images->first();
-        $productImageUrl = $principalImg ? asset($principalImg->photo_dir . $principalImg->photo) : null;
-
         foreach ($product->activePvas as $pva) {
             $pvaImageUrl = null;
             if ($pva->images && $pva->images->isNotEmpty()) {
@@ -913,8 +910,8 @@ class NextController extends Controller
                         if ($key === 'colors') {
                             if ($pvaImageUrl) {
                                 $attributesGrouped[$key][$attribute->id]['image'] = $pvaImageUrl;
-                            } elseif (!isset($attributesGrouped[$key][$attribute->id]['image'])) {
-                                $attributesGrouped[$key][$attribute->id]['image'] = $productImageUrl;
+                            } elseif (!array_key_exists('image', $attributesGrouped[$key][$attribute->id])) {
+                                $attributesGrouped[$key][$attribute->id]['image'] = null;
                             }
                         }
                     }
