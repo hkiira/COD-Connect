e89413409cf8bf25f24bb0f2f59bc5a8fe802ff6
hkiira <achkar.abder@gmail.com>
2026-08-04 01:14:21 +0100

    fix(cors): allow all origins and paths in cors configuration

--- config/cors.php
+++ config/cors.php
@@ -15,15 +15,10 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => [
-        'https://*.codconnect.cloud',
-        'https://*.codconnect.com',
-        'http://localhost:*',
-        'http://105.72.146.220:*'
-    ],
+    'allowed_origins_patterns' => ['.*'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
