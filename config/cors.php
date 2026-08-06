d41f1f142b93571d0317e1d8c0165e8236a673b9
hkiira <achkar.abder@gmail.com>
2026-08-06 16:48:58 +0100

    fix(cors): allow all origins and paths in cors configuration

--- config/cors.php
+++ config/cors.php
@@ -15,12 +15,12 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => ['.*'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => true,
+    'exposed_headers' => ['*'],
+    'max_age' => 86400,
+    'supports_credentials' => false,
 ];
